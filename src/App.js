7ef7b1b30123aba0ced572eb46c82f26e83069bc
Ethan <et.l.nicholas@gmail.com>
2023-01-06 11:58:41 +1100

    rm the not used postcss.config.js and add red color to text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="mt-5 ml-10 font-bold underline">
+    <div className="mt-5 ml-10 font-bold underline text-red-500">
       This is a shopping website.
     </div>
   );

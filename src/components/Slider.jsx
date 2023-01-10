c7612a0d112306478f8d31943578275b9d67f4ae
Ethan <et.l.nicholas@gmail.com>
2023-01-11 09:25:47 +1100

    add first slide and description

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -10,7 +10,21 @@ function Slider() {
             <ArrowLeftOutlined style={{fontSize: '50px'}}/>
         </div>
     {/* slide div */}
-        <div className='slide'></div>
+        <div className='wrapper flex w-[100%] h-[500px] justify-center items-center shadow-2xl rounded-lg border-[#c0c0c0] border-10px overflow-hidden relative bg-[#006171]'>
+          <div className='slide flex items-center justify-center h-[100%]'>
+            <div className='forImage flex flex-1 justify-center items-center h-[100%] object-cover'>
+              <img 
+                src ='https://images.unsplash.com/photo-1529139574466-a303027c1d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+                alt='slide__image'
+              />
+            </div>
+            <div className='des flex flex-col flex-1 place-items-start justify-center -ml-11'>
+              <h2 className='text-[55px] text-white'>Hey this is an awsome deal today</h2>
+                <p className='text-[30px] text-white'>Upto 30% off</p>
+                <button className='btn'>Shop Now</button>
+            </div>
+          </div>
+        </div>
     {/* rightarrow div */}
     <div className={arrowStyle}>
             <ArrowRightOutlined style={{fontSize: '50px'}}/>

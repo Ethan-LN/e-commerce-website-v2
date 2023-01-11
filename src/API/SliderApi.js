95f649d71a3330c727cccc58d1f0b096daaad74a
Ethan <et.l.nicholas@gmail.com>
2023-01-11 11:39:45 +1100

    slide show api

---
+++ src/API/SliderApi.js
@@ -0,0 +1,26 @@
+export const ApiSlides = [
+    {
+        src:"https://images.unsplash.com/photo-1529139574466-a303027c1d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        content: {
+            h2:"Hey this is an awsome deal today",
+            p: "Upto 30% off",
+        },
+        background: 'bg-[#006171]'
+    },
+    {
+        src:"https://images.unsplash.com/photo-1530735038726-a73fd6e6a349?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        content: {
+            h2:"Amazing Deals this Summer",
+            p: "Upto 30% off",
+        },
+        background: 'bg-[#277A9C]'
+    },
+    {
+        src:"https://images.unsplash.com/photo-1520367445093-50dc08a59d9d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+        content: {
+            h2:"Awesome Deals this week! ",
+            p: "Upto 30% off",
+        },
+        background: 'bg-[#642724]'
+    }
+]

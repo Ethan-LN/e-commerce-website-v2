6a9eb23cd6dc9750dcbd86ee07e58ecd8fef3240
Ethan <et.l.nicholas@gmail.com>
2023-01-11 15:36:53 +1100

    add product categories

---
+++ src/API/CategoryApi.js
@@ -0,0 +1,14 @@
+export const ApiCategories = [
+    {
+        title : "Jeans",
+        src: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        title : "Jackets",
+        src:"https://images.unsplash.com/photo-1544022613-e87ca75a784a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    },
+    {
+        title: "Style Tee Shirts",
+        src:"https://images.unsplash.com/photo-1583744946564-b52ac1c389c8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+    }
+]

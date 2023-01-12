dc910d68bab6c2281a31e5a706e0ac8e52ea0075
Ethan <et.l.nicholas@gmail.com>
2023-01-12 16:22:09 +1100

    use prettier formatting

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-function Category({item}) {
+function Category({ item }) {
   return (
-    <div className='flex-1 m-2 shadow-lg rounded-md overflow-hidden relative'>
-        <img src={item.src} alt='category_image' className=' w-[100%]'/>
-        <div className='flex absolute w-[100%] h-[100%] left-0 top-0 items-center justify-center flex-col'>
-            <h2 className='text-purple-400 font-medium text-[30px]'>{item.title}</h2>
-            <button className='btn'>See more</button>
-        </div>
+    <div className="flex-1 m-2 shadow-lg rounded-md overflow-hidden relative">
+      <img src={item.src} alt="category_image" className=" w-[100%]" />
+      <div className="flex absolute w-[100%] h-[100%] left-0 top-0 items-center justify-center flex-col">
+        <h2 className="text-purple-400 font-medium text-[30px]">
+          {item.title}
+        </h2>
+        <button className="btn">See more</button>
+      </div>
     </div>
   );
 }
 
-export default Category
+export default Category;

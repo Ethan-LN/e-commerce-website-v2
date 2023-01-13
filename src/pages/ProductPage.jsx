f3136d8a0d1cfb8a63d31c6a3394ed04221c978a
Ethan <et.l.nicholas@gmail.com>
2023-01-14 00:25:35 +1100

    add quantity, counter and 'add to cart' button

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Announce from '../components/Announce'
+import Counter from '../components/Counter'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
@@ -21,12 +22,25 @@ function ProductPage() {
                 Thisdfusdf dfdiud Suer kiodfdo fdgdlfjd dfdlfseref  dfodfis dfte fidht dfodfd! cvgfg trtfvf dkifd jdfs fkdsdfdf kioosidfs llldood fdkjd</p>
                 <p className='mt-7 text-xl'>Price: <b>$70 AUD</b></p>
                 {/*color variants  */}
-                <div className='flex text-xl gap-2'>
+                <div className='flex text-xl gap-2 mt-7'>
                     Colors
-                    <div className='bg-black w-[2rem] h-[2rem] rounded-full'></div>
-                    <div className='bg-gray-400 w-[2rem] h-[2rem] rounded-full'></div>
-                    <div className='bg-white w-[2rem] h-[2rem] border-2 border-gray-300 rounded-full'></div>
+                    <div className='bg-black w-[2rem] h-[2rem] rounded-full border-2 cursor-pointer'></div>
+                    <div className='bg-gray-400 w-[2rem] h-[2rem] border-2 rounded-full cursor-pointer'></div>
+                    <div className='bg-white w-[2rem] h-[2rem] border-2 rounded-full cursor-pointer'></div>
                 </div>
+                <div className='mt-7 text-xl '>
+                    Size
+                    <select className='ml-7 border-2 rounded-md'>
+                        <option selected disabled>Select</option>
+                        <option>Small</option>
+                        <option>Medium</option>
+                        <option>Large</option>
+                    </select>
+                </div>
+                <div className='mt-7'>
+                <Counter/> 
+                </div>
+                <button className='btn w-[180px] mt-7'>Add to cart</button>      
             </div>     
         </div>
         <Newsletter/>

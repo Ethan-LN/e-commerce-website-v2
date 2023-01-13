70f97447622e09a3519a7998c6a4f0e8280a31d5
Ethan <et.l.nicholas@gmail.com>
2023-01-13 17:57:43 +1100

    add hover effect on product

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,12 +1,20 @@
 import { FavoriteBorderOutlined, SearchOutlined, ShoppingCart, ShoppingCartOutlined } from '@mui/icons-material'
-import React from 'react'
+import { rgbToHex } from '@mui/material';
+import React, { useState } from 'react'
 
 function Product({item}) {
+    const [hoverEffect, SetHoverEffect] = useState(' opacity-0');
+    const handelHoverEnter = () => {
+        SetHoverEffect(' opacity-1 bg-[rgba(0,0,0,0.2)]');
+    }
+    const handelHoverExit = () => {
+        SetHoverEffect(' opacity-0');
+    }
     const iconStyle = 'h-[40px] w-[40px] rounded-full bg-white flex items-center justify-center cursor-pointer'
   return (
-    <div className='flex flex-1 items-center justify-center min-w-[190px] max-w-[220px] overflow-hidden rounded-md shadow-lg m-2 relative'>
+    <div className='flex flex-1 items-center justify-center min-w-[190px] max-w-[220px] overflow-hidden rounded-md shadow-lg m-2 relative' onMouseEnter={handelHoverEnter} onMouseLeave={handelHoverExit}>
         <img src={item.src} alt='product_image'/>
-        <div className='flex items-center justify-center w-[100%] h-[100%] absolute gap-3'>
+        <div className={`flex items-center justify-center w-[100%] h-[100%] absolute gap-3` + hoverEffect }>
             <div className={iconStyle}>
                 <ShoppingCartOutlined/>
             </div>

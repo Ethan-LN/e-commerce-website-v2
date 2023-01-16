fd4810a65b03550b1e39e25abc7d33de16e18ea8
Ethan <et.l.nicholas@gmail.com>
2023-01-16 21:04:54 +1100

    adjust product content

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -31,19 +31,19 @@ function Cart() {
                                 src='https://cdn.shopify.com/s/files/1/0240/7285/products/VisitorsCrewneckSweatshirtinWhite11_360x.jpg?v=1642719779'
                                 alt='product_img'/>
                         </div>
-                        <div className='sm:flex'>
+                        <div className='description sm:flex ml-5 h-auto justify-between'>
                             <div>
-                                <p>
+                                <p className='mb-3'>4
                                     <b>ID:</b> S3476543
                                 </p>
-                                <p>
+                                <p className='mb-3'>
                                     <b>Product:</b> White crew neck sweat shirt
                                 </p>
-                                <p className='flex gap-3'>
+                                <p className='flex gap-3 items-center justify-start mb-3'>
                                     <b>Color:</b> 
                                     <div className="bg-white w-[1.5rem] h-[1.5rem] border-2 rounded-full cursor-pointer hover:border-[#8a4af3]"></div>
                                 </p>
-                                <p>
+                                <p className='mb-3'>
                                     <b>Size:</b> Medium
                                 </p>
                             </div>                      

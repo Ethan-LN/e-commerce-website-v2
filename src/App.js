292847918cbb8aca1737fa23f5b0bf5204ae52f1
Ethan <et.l.nicholas@gmail.com>
2023-01-23 17:53:47 +1100

    add mobile resposive to the category page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Cart from './pages/Cart';
 import CategoryPage from './pages/CategoryPage';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import ProductPage from './pages/ProductPage';
 import Register from './pages/Register';
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className='App'>
       <div className='mt-5 ml-5'>
-      <Home/>
+      <CategoryPage/>
       </div>
     </div>
   );

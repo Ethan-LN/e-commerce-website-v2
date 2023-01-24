c1436667b492bdd99663e85698b5bc8659e99e04
Ethan <et.l.nicholas@gmail.com>
2023-01-24 16:57:45 +1100

    add the direction of pages to the buttons and texts

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Announce from "../components/Announce";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 export default class Login extends React.Component {
   state = {
@@ -12,34 +15,39 @@ export default class Login extends React.Component {
   };
   render() {
     return (
-      <div className="flex justify-center">
-        <div className="flex flex-col absolute top-[30%] shadow-lg border-[2px] rounded-lg w-[60%] p-5 mobile:w-[90%] mr-5">
-          <text className="text-2xl">Login</text>
-          <div className="flex mt-3">
+      <div>
+        <Announce />
+        <Navbar />
+        <div className="flex justify-center">
+          <div className="flex flex-col absolute top-[30%] shadow-lg border-[2px] rounded-lg w-[60%] p-5 mobile:w-[90%] mr-5">
+            <text className="text-2xl">Login</text>
+            <div className="flex mt-3">
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                placeholder="username"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className="flex mt-3">
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                name="password"
+                placeholder="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <Link to="/" className="btn mt-7 hover:scale-[1.02] text-center">
             <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              placeholder="username"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleChange}
-              required
+              type="button"
+              value="login"
             />
+            </Link>
           </div>
-          <div className="flex mt-3">
-            <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              name="password"
-              placeholder="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <input
-            type="button"
-            value="login"
-            className="btn mt-7 hover:scale-[1.02]"
-          />
         </div>
       </div>
     );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Announce from "../components/Announce";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
 
 export default class Register extends React.Component {
   state = {
@@ -15,62 +18,69 @@ export default class Register extends React.Component {
   };
   render() {
     return (
-      <div className="flex justify-center">
-        <div className="flex flex-col absolute top-[30%] shadow-lg border-[2px] rounded-lg w-[60%] p-5 mobile:w-[90%] mr-5">
-          <text className="text-2xl">Register</text>
-          <div className="flex mt-3 gap-3">
+      <div>
+        <Announce />
+        <Navbar />
+        <div className="flex justify-center">
+          <div className="flex flex-col absolute top-[30%] shadow-lg border-[2px] rounded-lg w-[60%] p-5 mobile:w-[90%] mr-5">
+            <text className="text-2xl">Register</text>
+            <div className="flex mt-3 gap-3">
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                placeholder="first name"
+                name="firstname"
+                value={this.state.firstname}
+                onChange={this.handleChange}
+                required
+              />
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                placeholder="last name"
+                name="lastname"
+                value={this.state.lastname}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className="flex mt-3">
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                placeholder="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className="flex mt-3">
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                name="password"
+                placeholder="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            <div className="flex mt-3">
+              <input
+                className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
+                name="confirmpass"
+                placeholder="confirm password"
+                value={this.state.confirmpass}
+                onChange={this.handleChange}
+                required
+              />
+            </div>
+            {/* Temperate Link to Home page */}
+            <Link to="/" className="btn mt-7 hover:scale-[1.02] text-center">
             <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              placeholder="first name"
-              name="firstname"
-              value={this.state.firstname}
-              onChange={this.handleChange}
-              required
-            />
-            <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              placeholder="last name"
-              name="lastname"
-              value={this.state.lastname}
-              onChange={this.handleChange}
-              required
-            />
+              to = "/"
+              type="button"
+              value="Register"
+              />
+            </Link>
           </div>
-          <div className="flex mt-3">
-            <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              placeholder="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="flex mt-3">
-            <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              name="password"
-              placeholder="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div className="flex mt-3">
-            <input
-              className="border-[2px] rounded-lg w-[100%] p-2 outline-[#8a4af3] focus:border-[#8a4af3] ease-linear duration-200"
-              name="confirmpass"
-              placeholder="confirm password"
-              value={this.state.confirmpass}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <input
-            type="button"
-            value="Register"
-            className="btn mt-7 hover:scale-[1.02]"
-          />
         </div>
       </div>
     );
